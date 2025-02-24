63e81cdee6c929dcf93fc4904ec5d4be8c28e90a
Sk Soyeb Akhter <sksoyebakhter2003@gmail.com>
2025-02-25 02:01:28 +0530

    added google analytics using script

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import { ClerkProvider } from "@clerk/nextjs"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import Head from "next/head"
-import { GoogleAnalytics } from '@next/third-parties/google'
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
@@ -15,7 +14,6 @@ export default function App({ Component, pageProps }) {
           <title>Dont get leaked</title>
         </Head>
         <Component {...pageProps} />
-        <GoogleAnalytics gaId="G-BBDP5JNMC0" />
         <Toaster />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,28 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BBDP5JNMC0"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BBDP5JNMC0');
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
